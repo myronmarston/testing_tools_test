925b50721c8f3e750cd1d49b83a8a84299621073
Myron Marston <myron.marston@gmail.com>
2009-11-18 14:04:51 -0800

    Added debugging support during spec runs.

---
+++ spec/support/debugging.rb
@@ -0,0 +1,8 @@
+# enable debugger
+begin
+  require 'ruby-debug'
+  Debugger.start
+  Debugger.settings[:autoeval] = true if Debugger.respond_to?(:settings)
+rescue LoadError
+  # ruby-debug wasn't available so neither can the debugging be
+end
